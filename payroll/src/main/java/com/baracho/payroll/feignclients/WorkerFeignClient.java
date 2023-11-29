3c11228344bac8011f6cb3bde4014b413e38bdbf
Baracho <baracholss@gmail.com>
2023-11-29 19:04:37 -0300

    Feign Client

---
+++ payroll/src/main/java/com/baracho/payroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,18 @@
+package com.baracho.payroll.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.baracho.payroll.entities.Worker;
+
+@Component
+@FeignClient(name = "worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+	
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+
+}
--- payroll/src/main/java/com/baracho/payroll/services/PaymentServices.java
+++ payroll/src/main/java/com/baracho/payroll/services/PaymentServices.java
@@ -1,32 +1,23 @@
 package com.baracho.payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.baracho.payroll.entities.Payment;
 import com.baracho.payroll.entities.Worker;
+import com.baracho.payroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentServices {
 	
-	@Value("${worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
 		
-		Map<String, String> uriVariables = new HashMap<>();
 		
-		uriVariables.put("id", ""+workerId);
 		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
